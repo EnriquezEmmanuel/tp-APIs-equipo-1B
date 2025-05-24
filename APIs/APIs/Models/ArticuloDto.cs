5c93a5ba4a600585c3cebc9830140d0ae22ceca9
@Emma6891 <enriquez.eml@gmail.com>
2025-05-24 16:30:13 -0300

    Algunos arreglos iniciales

---
+++ APIs/APIs/Models/ArticuloDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Dominio;
+
+namespace APIs.Models
+{
+    public class ArticuloDto
+    {
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int Marca { get; set; }
+        public int Categoria { get; set; }
+        public decimal Precio { get; set; }
+
+        // Nueva propiedad: Lista de imágenes asociadas
+        public List<Imagen> Imagenes { get; set; } = new List<Imagen>();
+    }
+}
--- APIs/Negocio/ArticuloNegocio.cs
+++ APIs/Negocio/ArticuloNegocio.cs
@@ -49,5 +49,85 @@ namespace Negocio
                 datos.cerrarConexion();
             }
         }
+        public void AgregarArticulo(Articulo nuevo)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("BEGIN TRANSACTION INSERT ARTICULOS(Codigo, Nombre, Descripcion, IdMarca, IdCategoria, Precio) OUTPUT INSERTED.ID VALUES (@Codigo, @Nombre, @Descripcion, @IdMarca, @IdCategoria, @Precio) DECLARE @ID INT= SCOPE_IDENTITY() INSERT INTO IMAGENES(IdArticulo, ImagenUrl) VALUES (@ID,@ImagenUrl1 ), (@ID,@ImagenUrl2 ), (@ID,@ImagenUrl3 )COMMIT;");
+                datos.setearParametro("@Codigo", nuevo.Codigo);
+                datos.setearParametro("@Nombre", nuevo.Nombre);
+                datos.setearParametro("@Descripcion", nuevo.Descripcion);
+                datos.setearParametro("@IdMarca", nuevo.Marca.Id);
+                datos.setearParametro("@IdCategoria", nuevo.Categoria.Id);
+                datos.setearParametro("@Precio", nuevo.Precio);
+
+                //esto se puede mejorar, solo es para probar
+
+                datos.setearParametro("@ImagenUrl1", nuevo.Imagenes[0].Url);
+                datos.setearParametro("@ImagenUrl2", nuevo.Imagenes[1].Url);
+                datos.setearParametro("@ImagenUrl3", nuevo.Imagenes[2].Url);
+
+                datos.ejecutarAccion();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+
+        }
+        public void Modificar(Articulo art)
+        {
+            AccesoDatos datosModificados = new AccesoDatos();
+            try
+            {
+
+                datosModificados.setearParametro("@id", art.Id);
+                datosModificados.setearParametro("@cod", art.Codigo);
+                datosModificados.setearParametro("@nom", art.Nombre);
+                datosModificados.setearParametro("@desc", art.Descripcion);
+                datosModificados.setearParametro("@Mrca", art.Marca.Id);
+                datosModificados.setearParametro("@Ctgria", art.Categoria.Id);
+                datosModificados.setearParametro("@Prec", art.Precio);
+                //datosModificados.setearParametro("@img", art.UrlImagen);
+
+                datosModificados.setearParametro("@img1", art.Imagenes[0].Url);
+                datosModificados.setearParametro("@img2", art.Imagenes[1].Url);
+                datosModificados.setearParametro("@img3", art.Imagenes[2].Url);
+
+                datosModificados.setearConsulta("UPDATE IMAGENES SET ImagenUrl=@img1 WHERE Id=( SELECT MIN(Id) FROM IMAGENES WHERE IdArticulo =@id)");
+                datosModificados.ejecutarAccion();
+                datosModificados.cerrarConexion();
+                datosModificados.setearConsulta("UPDATE IMAGENES SET ImagenUrl=@img2 WHERE Id=( SELECT MIN(Id)+1 FROM IMAGENES WHERE IdArticulo =@id)");
+                datosModificados.ejecutarAccion();
+                datosModificados.cerrarConexion();
+                datosModificados.setearConsulta("UPDATE IMAGENES SET ImagenUrl=@img3 WHERE Id=( SELECT MIN(Id)+2 FROM IMAGENES WHERE IdArticulo =@id)");
+                datosModificados.ejecutarAccion();
+                datosModificados.cerrarConexion();
+
+                datosModificados.setearConsulta("UPDATE ARTICULOS SET IdMarca = @Mrca WHERE Id = @id");
+                datosModificados.ejecutarAccion();
+                datosModificados.cerrarConexion();
+                datosModificados.setearConsulta("UPDATE ARTICULOS SET IdCategoria = @Ctgria WHERE Id = @id");
+                datosModificados.ejecutarAccion();
+                datosModificados.cerrarConexion();
+                datosModificados.setearConsulta("UPDATE ARTICULOS SET Codigo=@cod, Nombre=@nom, Descripcion=@desc, Precio=@Prec WHERE Id=@id");
+                datosModificados.ejecutarAccion();
+                datosModificados.cerrarConexion();
+
+            }
+            catch (Exception ex)
+            { throw ex; }
+            //finally { datosModificados.cerrarConexion(); }
+
+        }
+
     }
 }
